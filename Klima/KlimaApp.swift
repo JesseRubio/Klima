1c1607bbcff0d20713abef72633fe728b0acadd7
Jess <jrubio87@hotmail.com>
2026-04-19 00:00:39 -0500

    Initial Commit

---
+++ Klima/KlimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlimaApp.swift
+//  Klima
+//
+//  Created by Jesse Rubio on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct KlimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
